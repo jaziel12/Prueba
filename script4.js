ddca7486b73b76dac595e273f11f83da4b98c76f
Jaziel Gutierrez <jazielgutierrez50@gmail.com>
2022-05-31 12:37:02 -0300

    DOM

---
+++ script4.js
@@ -0,0 +1,9 @@
+//Cambio el nombre
+let Titulo=document.getElementsByClassName("TT");
+TT.textContent = "JM"
+
+
+
+//creando elemtentos
+const section = document.getElementById("section");
+section.insertAdjacentText("beforeend","Estoy acá")
